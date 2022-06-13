dbf03b4fd137b1429da4dbdb8a2c7e47dec46f26
jay1224-jay <wuj20061224@gmail.com>
2022-06-13 15:42:10 +0800

    first commit

---
+++ minesweeper.c
@@ -0,0 +1,145 @@
+/*
+ *  to-do list
+ *  1. auto pand
+ *
+ *
+ * */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "minesweeper.h"
+
+
+int main() {
+
+    printf(KWHT);
+
+    system("clear");
+
+    printf("This is a minesweeper game by jay(or wuj).\n \
+how to play:\n\
+    1. type how many bombs you want\n\
+    2. type the x-coordinate, y-coordinate, and mode ( \"a\" for activate and \"f\" forflag ) \n\
+    example: if you want the position of (2, 3) and to activate it, type \"2 3 a\" and press Enter\n\
+                                          x, y \n\
+    3. ENJOY!!\n\n\
+");
+
+    
+
+
+
+    int number_bombs;
+    
+    printf("how many bombs do you want: ");
+    scanf("%d", &number_bombs);
+
+    if (number_bombs > COLUMNS * RAWS) {
+        printf("too large number! must be smaller than %d * %d!\n", COLUMNS, RAWS);
+        return 0;
+    }
+
+    system("clear");
+    
+    // plant bombs
+    int b=0;
+    srand(time(0));
+    while ( b < number_bombs ) {
+        int rand_x = rand() % RAWS; // 0~8
+
+        int rand_y = rand() % COLUMNS;
+
+        if ( map[rand_y][rand_x][0] == 0 ) {
+            map[rand_y][rand_x][0] = -1;
+            b++;
+        }
+    }
+    // plant bombs
+
+    // === set number ===
+    // printf("\n%d\n", number_of_near_bombs(pos, map));
+
+
+    for (int y=0;y<COLUMNS;y++) {
+        for (int x=0;x<RAWS;x++) {
+            if ( map[y][x][0] == 0  ) 
+                map[y][x][0] = number_of_near_bombs(x, y, map);
+        }
+    }
+
+    // === set number ===
+    
+    
+    // game loop
+    /*
+    activate_all(ptr_map);
+    show_map(map);
+
+    puts("");
+
+    for (int y=0;y<COLUMNS;y++) {
+        for (int x=0;x<RAWS;x++)
+            *ptr_map[y][x][1] = 0;
+    } */
+
+    while (1) {
+
+        
+
+        show_map(map);
+
+        int input_x,   input_y;
+        char mode;  // 'a' for activate and 'f' for flag
+
+        printf("where and mode: ");
+        scanf("%d %d %c", &input_x, &input_y, &mode);
+
+        switch ( mode ) {
+
+            case 'a':
+        
+                if ( map[input_y][input_x][0] == -1) { // current posion is bomb
+                    printf("you just activate a bomb!\n");
+
+                    // activate all
+
+                    activate_all( map );
+                    show_map(map);
+                    
+                    return 0; // end this game
+
+                }
+
+                activate(input_x, input_y, map);
+                break; // stop case 'a'
+
+            case 'f':
+                flag(input_x, input_y, map);
+                break;
+
+            default:
+                printf("%c is wrong input \n", mode);
+                return 0;
+        
+        }
+
+        if ( check_win(map) ) {
+            activate_all(map);
+            puts("");
+            show_map(map);
+            puts("");
+            printf(" ======== you win ========\n");
+            return 0;
+        }
+
+        system("clear");
+
+    }
+
+
+    return 1;
+}
+
---
+++ minesweeper.h
@@ -0,0 +1,177 @@
+/*
+ * today is 2022/6/9
+ * raw and column
+ *   raw1, raw2, raw3
+ * c1
+ * c2
+ * c3
+ * 
+ * -1 means bomb
+ * 
+*/
+
+#define RAWS    20
+#define COLUMNS 20
+
+// color
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+// number
+// [3, 0]
+//     activate , flag, or not
+//        1        2        0
+//
+
+typedef int map_type[COLUMNS][RAWS][2];
+
+int map[COLUMNS][RAWS][2];
+
+void show_map( map_type map );
+int number_of_near_bombs(int x, int y, map_type map);
+void activate(int x, int y, map_type map);
+void activate_all( map_type map );
+int check_win( map_type map );
+
+
+void show_map(map_type map) {
+
+    printf("    ");
+    for (int index_x=0;index_x<RAWS;index_x++) {
+        printf("%2d ", index_x);
+    }
+    puts("");
+
+    for(int y=0;y<COLUMNS;y++) {
+
+        printf(" %2d ", y);
+
+        for (int x=0;x<RAWS;x++) {
+            if ( map[y][x][1] == 1 ) {
+                if ( map[y][x][0] == -1) { 
+                    printf( KGRN "%2d " KWHT, -1);
+                    continue;
+                }
+                printf( KWHT "%2d " KWHT , map[y][x][0]);
+            } else if ( map[y][x][1] == 2 ) {
+                printf( KBLU " F " KWHT );
+            } else {
+                printf( KWHT " - " KWHT );
+            }
+        }
+
+        puts("");
+        
+    }
+}
+
+
+/*
+
+relative postion:
+    -1  0  1
+  -1 1  2  3
+  0  4     5
+  1  6  7  8
+
+curr_pos = (x, y)
+*/
+
+int number_of_near_bombs(int x, int y, map_type map) {
+
+
+    int number = 0;
+
+    if ( (y-1) >= 0 ) {
+        // 1
+        if ( (x-1) >= 0 ) {
+            if ( map[y-1][x-1][0] == -1)
+                number++;
+        }
+            
+            // 2
+        if ( map[y-1][x][0] == -1)
+            number++;
+
+            // 3
+        if ( (x+1) < RAWS) {
+
+            if ( map[y-1][x+1][0] == -1)
+                number++;
+        }
+    }
+
+        // 4
+    if ( (x-1) >= 0 ) {
+        if ( map[y][x-1][0] == -1)
+            number++;
+    }
+
+    // 5
+    if ( (x+1) < RAWS ) {
+        if ( map[y][x+1][0] == -1)
+            number++;
+    }
+
+
+    if ( (y+1) < COLUMNS ) {
+        // 6
+        if ( (x-1) >= 0 ) { 
+            if ( map[y+1][x-1][0] == -1)
+                number++;
+        }
+
+        // 7
+        if ( map[y+1][x][0] == -1)
+            number++;
+
+        // 8
+        if ( (x+1) < RAWS) {
+            if ( map[y+1][x+1][0] == -1)
+                number++;
+        }
+    }
+
+
+    return number;
+
+}
+
+void activate(int x, int y, map_type map) {
+    map[y][x][1] = 1;
+}
+
+void activate_all( map_type map ) {
+
+    for (int y=0;y<COLUMNS;y++) {
+        for (int x=0;x<RAWS;x++)
+            map[y][x][1] = 1;
+    }
+}
+
+void flag( int x, int y, map_type map ) {
+    map[y][x][1] = 2; // flag mode
+}
+
+
+// the condition of win:  all of not bombs are activated
+
+int check_win( map_type map ) {
+
+    for (int y=0;y<COLUMNS;y++) {
+        for (int x=0;x<RAWS;x++) {
+            if (map[y][x][0] != -1 && map[y][x][1] == 0) { // 0 means not activated
+                return 0; // not yet win
+            }
+        }
+    }
+
+    return 1; // win
+
+}
